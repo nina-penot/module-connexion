cefbd4551c30b5248fba7cd0bc400794e4701251
nina-penot <nina.penot@laplateforme.io>
2025-10-03 15:34:56 +0200

    change sql

--- sql/moduleconnexion.sql
+++ sql/moduleconnexion.sql
@@ -33,7 +33,7 @@ CREATE TABLE `utilisateurs` (
   `prenom` varchar(255) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `utilisateurs`
